72a2e9f71703625ba35581c95a11ab90d83ab199
Cole <coleb229@yahoo.com>
2023-08-21 14:56:09 -0700

    Setup canvas & player movement

---
+++ index.js
@@ -0,0 +1,126 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2
+
+// creates a class for the player/enemy
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position
+    this.velocity = velocity
+    this.height = 150
+  }
+
+  draw() {
+    ctx.fillStyle = 'red'
+    ctx.fillRect(this.position.x, this.position.y, 50, this.height)
+  }
+
+  update() {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0
+    } else {
+      this.velocity.y += gravity
+    }
+  }
+}
+
+//initializes the player
+const player = new Sprite({
+  position: {
+    x: 100,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+})
+
+//initializes the enemy
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+})
+
+console.log(player)
+
+// creates pressed property for keys to smooth movement
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+}
+
+let lastkey
+
+// mainloop for the game
+function animate() {
+  window.requestAnimationFrame(animate)
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+
+  player.velocity.x = 0
+
+  if (keys.a.pressed && lastkey === 'a') {
+    player.velocity.x = -1
+  } else if (keys.d.pressed && lastkey === 'd') {
+    player.velocity.x = 1
+  }
+}
+
+animate()
+
+// listens for keypresses
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true
+      lastkey = 'd'
+      break
+    case 'a':
+      keys.a.pressed = true
+      lastkey = 'a'
+      break
+    case 'w':
+      player.velocity.y = -10
+      break
+  }
+  console.log(event.key)
+})
+
+// listens for key releases
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+  }
+  console.log(event.key)
+})
