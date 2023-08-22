998f8c5486a5c76ef0c1a12fba86bc8626d85129
Cole <coleb229@yahoo.com>
2023-08-22 11:48:28 -0700

    HP interface completed

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Sprite {
     }
     this.color = color
     this.isAttacking
+    this.health = 100
   }
 
   draw() {
@@ -166,7 +167,8 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false
-    console.log('player attack successful')
+    enemy.health -= 10
+    document.querySelector('#enemyHealth').style.width = enemy.health + '%'
   }
 
   if (
@@ -177,7 +179,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false
-    console.log('enemy attack successful')
+    player.health -= 10
+    document.querySelector('#playerHealth').style.width = player.health + '%'
   }
 }
 
